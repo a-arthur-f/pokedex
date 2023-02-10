8ad7ec163a42643bbc1dc34115d68c783fe9965f
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-10 20:48:14 -0300

    add header in main template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import Header from "./components/Header.vue";
 import { RouterView } from "vue-router";
 </script>
 
 <template>
+  <Header />
   <RouterView />
 </template>
 
