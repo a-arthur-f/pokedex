0323ff1f3be73d1b31cb4d2d65d41331907ae9ab
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-10 20:45:39 -0300

    feat(components): create button

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+defineProps<{ text: string; disabled?: boolean }>();
+</script>
+<template>
+  <button :disabled="disabled">{{ text }}</button>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/variables";
+button {
+  all: unset;
+  background-color: variables.$button;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: 300;
+  transition: all 0.5s;
+  box-sizing: border-box;
+  text-align: center;
+
+  &:hover {
+    background-color: variables.$button-hover;
+  }
+
+  &:disabled {
+    background-color: variables.$button-disabled;
+    cursor: default;
+  }
+}
+</style>
