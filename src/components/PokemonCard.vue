694e122af06b0a2867004087a1244c297554ef4b
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-10 19:24:05 -0300

    feat(card): add type badge in card

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import TypeBadge from "./TypeBadge.vue";
+
 defineProps<{ pokemon: Pokemon }>();
 function padID(id: number): string | number {
   return id < 999 ? id.toString().padStart(3, "0") : 0;
@@ -6,43 +8,61 @@ function padID(id: number): string | number {
 </script>
 
 <template>
-  <li :class="pokemon.types[0].type.name">
+  <li :class="'pokemon-card__container bg-' + pokemon.types[0].type.name">
     <figure>
       <img
         :src="pokemon.sprites.other.home.front_default"
         :alt="'image of ' + pokemon.name"
       />
     </figure>
-    <p>#{{ padID(pokemon.id) }}</p>
-    <p>{{ pokemon.name }}</p>
+    <p class="pokemon-card__id">#{{ padID(pokemon.id) }}</p>
+    <p class="pokemon-card__name">{{ pokemon.name }}</p>
+
+    <ul class="pokemon-card__type-list">
+      <li v-for="{ type: pokeType } of pokemon.types">
+        <TypeBadge :type="pokeType" />
+      </li>
+    </ul>
   </li>
 </template>
 
 <style lang="scss" scoped>
-li {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-  font-size: 3rem;
-  color: #fff;
-  padding-bottom: 1rem;
-  font-weight: 600;
+.pokemon-card {
+  &__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    color: #fff;
+    padding-bottom: 1rem;
+    font-weight: 600;
 
-  figure {
-    width: 16rem;
-    height: 16rem;
-    margin-bottom: 2rem;
+    figure {
+      width: 14rem;
+      height: 14rem;
+      margin-bottom: 2rem;
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+
+    p {
+      text-transform: uppercase;
+    }
   }
 
-  img {
-    width: 100%;
-    height: 100%;
+  &__name {
+    font-size: 2rem;
   }
 
-  p {
-    text-transform: uppercase;
+  &__type-list {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    margin-top: 0.5rem;
   }
 }
 </style>
