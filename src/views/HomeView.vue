54be182da2464bb4d7278eb9708907ad82f5e266
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-10 20:52:41 -0300

    feat(views): home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainContainer from "@/components/MainContainer.vue";
+import Button from "@/components/Button.vue";
+import { usePokedexStore } from "../stores/pokedex";
+import { storeToRefs } from "pinia";
+import PokemonList from "@/components/PokemonList.vue";
+
+const store = usePokedexStore();
+
+const { pokedex, next, previous, loading } = storeToRefs(store);
+const { fetchPokemons } = store;
+</script>
 
 <template>
-  <main>
-    <h1>Hello World</h1>
-  </main>
+  <MainContainer v-if="!loading" class="home__container">
+    <PokemonList :pokemons="pokedex" />
+    <div class="home__button-container">
+      <Button
+        :disabled="!previous"
+        @click="fetchPokemons(previous)"
+        text="previous"
+      />
+      <Button :disabled="!next" @click="fetchPokemons(next)" text="next" />
+    </div>
+  </MainContainer>
 </template>
+
+<style lang="scss" scoped>
+.home {
+  &__container {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  &__button-container {
+    display: flex;
+    justify-content: center;
+    gap: 4rem;
+
+    button {
+      min-width: 126px;
+    }
+  }
+}
+</style>
