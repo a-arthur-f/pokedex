865db6a2c972f8891702a57191535a5db371e5a8
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-09 22:02:54 -0300

    feat(components): create main container

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  padding: 0 1rem;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    padding: 0 4rem;
+  }
+}
+</style>
