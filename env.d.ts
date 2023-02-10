381c40e305acecff1f0d1323a517b7786a5b47fa
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-09 21:45:48 -0300

    feat(types): common types

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,44 @@
 /// <reference types="vite/client" />
+
+interface Pokemon {
+  id: number;
+  name: string;
+  types: PokemonType[];
+  sprites: PokemonSprites;
+  home: PokemonSprites;
+  height: number;
+  weight: number;
+  stats: PokemonStat[];
+}
+
+interface PokemonStat {
+  stat: NamedApiResource;
+  effort: number;
+  base_stat: number;
+}
+
+interface PokemonType {
+  slot: number;
+  type: NamedApiResource;
+}
+
+interface Type {
+  id: number;
+  name: string;
+}
+
+interface PokemonSprites {
+  front_default: string;
+  front_shiny: string;
+  other: {
+    home: {
+      front_default: string;
+      front_shiny: string;
+    };
+  };
+}
+
+interface NamedApiResource {
+  name: string;
+  url: string;
+}
