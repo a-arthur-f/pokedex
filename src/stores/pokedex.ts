2a78b1025f5f1a3da76feb2a927b7b4f1558e906
Arthur Fusco <fuscoarthur1@gmail.com>
2023-02-09 21:46:45 -0300

    feat(store): create pokedex store

---
+++ src/stores/pokedex.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import { onMounted, ref } from "vue";
+
+export const usePokedexStore = defineStore("pokedex", () => {
+  const pokedex = ref<Pokemon[]>([]);
+  const count = ref(0);
+  const next = ref("");
+  const previous = ref("");
+
+  async function fetchPokemons(url: string) {
+    try {
+      const {
+        results: pokemons,
+        count: total,
+        next: nextUrl,
+        previous: previousUrl,
+      } = (await axios<FetchData>(url)).data;
+
+      count.value = total;
+      next.value = nextUrl;
+      previous.value = previousUrl;
+      pokedex.value = [];
+
+      for (let p of pokemons) {
+        const { data: pokemon } = await axios<Pokemon>(p.url);
+        pokedex.value = [...pokedex.value, pokemon];
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  onMounted(() =>
+    fetchPokemons("https://pokeapi.co/api/v2/pokemon?limit=21&offset=1200")
+  );
+
+  return { pokedex, previous, next, fetchPokemons };
+});
+
+interface FetchData {
+  count: number;
+  next: string;
+  previous: string;
+  results: NamedApiResource[];
+}
